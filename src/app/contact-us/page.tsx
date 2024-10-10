819e4779cc4519b3065398c2a8236bdc92b2f96b
Hammad Zamir <annibaba009@gmail.com>
2024-10-10 15:40:22 +0500

    contact-us issue resolved

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 import PageIntroduction from "../components/generic/page-introduction";
 import { ContactInfo } from "../../../types";
 import Form from "./Form";
@@ -54,10 +54,95 @@ const ContactUs = async () => {
       <PageIntroduction title="Contact Us" image="/contact-us.png" />
 
       {/* Contact Information Section */}
-      <div className="flex flex-col items-stretch justify-center gap-4 lg:flex-row lg:items-stretch xl:gap-16">
+      <div className="flex flex-col items-stretch  justify-center gap-4 lg:flex-row lg:items-stretch xl:gap-16">
+
         {/* Contact Info */}
-        <div className=" relative w-full lg:w-1/2 flex flex-col gap-8 bg-gray-100 p-4 px-16 py-8 md:py-12 lg:px-8 lg:py-16 xl:!px-[60px] xl:py-20">
-          {/* Contact details */}
+        <div className="relative w-full lg:w-1/2 flex flex-col gap-8 bg-gray-100 p-4 px-16 py-8 md:py-12 lg:px-8 lg:py-16 xl:!px-[60px] xl:py-20">
+          <div className="absolute right-0 top-0 hidden h-full w-[50px] py-24 xl:block">
+            <div className="h-full w-full bg-primary/70"></div>
+          </div>
+          <div>
+            <h2 className="landing-page-heading">Get in Touch with Us</h2>
+          </div>
+          <div className="border-l-4 border-primary pl-8">
+            <h3 className="mb-2 text-primary md:text-lg lg:text-xl xl:text-[25px]">
+              Contact Number:
+            </h3>
+            <p className="md:text-lg lg:text-xl xl:text-[25px]">
+              {contactInfo?.primaryContact}
+            </p>
+          </div>
+          <div className="border-l-4 border-primary pl-8">
+            <h3 className="mb-2 text-primary md:text-lg lg:text-xl xl:text-[25px]">
+              Email:
+            </h3>
+            <p className="md:text-lg lg:text-xl xl:text-[25px]">
+              {contactInfo?.primaryEmail}
+            </p>
+          </div>
+          <div className="border-l-4 border-primary pl-8">
+            <h3 className="mb-2 text-primary md:text-lg lg:text-xl xl:text-[25px]">
+              Office Address:
+            </h3>
+            <p className="md:text-lg lg:text-xl xl:text-[25px]">
+              {contactInfo?.primaryAddress}
+            </p>
+          </div>
+          <div className="flex items-center justify-center gap-4 sm:justify-start xl:gap-6 2xl:gap-8">
+            {/* Social Media Icons */}
+            <a
+              href={contactInfo?.facebook}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/Social Icons/purple/facebook.png"
+                alt="facebook logo"
+                width={100}
+                height={100}
+                className="h-7 w-7 sm:h-9 sm:w-9 lg:h-11 lg:w-11"
+              />
+            </a>
+            <a
+              href={contactInfo?.instagram}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/Social Icons/purple/instagram-2.png"
+                alt="instagram logo"
+                width={100}
+                height={100}
+                className="h-7 w-7 sm:h-9 sm:w-9 lg:h-11 lg:w-11"
+              />
+            </a>
+            <a
+              href={contactInfo?.linkedIn}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/Social Icons/purple/linkedin.png"
+                alt="linkedin logo"
+                width={100}
+                height={100}
+                className="h-7 w-7 sm:h-9 sm:w-9 lg:h-11 lg:w-11"
+              />
+            </a>
+            <a
+              href={contactInfo?.youtube}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/Social Icons/purple/youtube.png"
+                alt="youtube logo"
+                width={100}
+                height={100}
+                className="h-7 w-7 sm:h-9 sm:w-9 lg:h-11 lg:w-11"
+              />
+            </a>
+          </div>
         </div>
 
         {/* Form */}
