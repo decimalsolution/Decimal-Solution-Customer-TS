713296d2673147acd84189461fb92a790b8f86a8
Owais Mehmood <sultanowaismehmood@gmail.com>
2025-03-19 01:51:28 +0500

    Refactor ServicesContent component for improved code readability and consistency

--- src/app/services/servicescontent.tsx
+++ src/app/services/servicescontent.tsx
@@ -1,33 +1,28 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 import { Service } from "../../../types";
-import ServiceCard from '../components/generic/service-card';
-import { useState , useMemo } from 'react';
-import { cn } from '../../../lib/utils';
-
+import ServiceCard from "../components/generic/service-card";
+import { useState, useMemo } from "react";
+import { cn } from "../../../lib/utils";
 
 interface ServicesContentProps {
-    services: Service[];
-    ServiceBar : string[];
+  services: Service[];
+  ServiceBar: string[];
 }
 
-const ServicesContent:React.FC<ServicesContentProps> = ({services , ServiceBar} ) => {
-
+const ServicesContent: React.FC<ServicesContentProps> = ({ services, ServiceBar }) => {
   const [selected, setSelected] = useState<string>("All");
 
   const filteredServices = useMemo(() => {
     if (selected === "All") {
       return services;
     } else {
-      return services.filter(
-        (item) =>
-          item.title?.toLowerCase() === selected.toLowerCase(),
-      );
+      return services.filter((item) => item.title?.toLowerCase() === selected.toLowerCase());
     }
   }, [selected, services]);
 
-//   console.log(filteredServices)
+  //   console.log(filteredServices)
 
   const getLink = (service: Service) => {
     const title = service.title.toLowerCase();
@@ -47,7 +42,7 @@ const ServicesContent:React.FC<ServicesContentProps> = ({services , ServiceBar} 
       return "services/game-development";
     } else if (title.includes("content")) {
       return "services/content-creation";
-    }  else if (title.includes("management")) {
+    } else if (title.includes("management")) {
       return "services/management-services";
     } else {
       return "services/website-development";
@@ -55,14 +50,14 @@ const ServicesContent:React.FC<ServicesContentProps> = ({services , ServiceBar} 
   };
 
   return (
-    <div >
-       <div className=" flex flex-wrap items-center  justify-center gap-2 sm:gap-4">
+    <div>
+      <div className=" flex flex-wrap items-center  justify-center gap-2 sm:gap-4">
         {ServiceBar.map((service: string, index: number) => (
           <button
             key={"our-projects-buttons-" + index + "-key"}
             className={cn(
               "rounded-lg border px-4 py-2 text-xs transition-all duration-200 hover:bg-primary hover:text-white sm:text-sm md:text-base lg:text-md",
-              selected === service? "bg-primary text-white" : ""
+              selected === service ? "bg-primary text-white" : ""
             )}
             onClick={() => {
               setSelected(service);
@@ -70,29 +65,29 @@ const ServicesContent:React.FC<ServicesContentProps> = ({services , ServiceBar} 
           >
             {service}
           </button>
-         ))}
-       </div>
-
-      <div className="flex flex-col gap-8 ">
-        {filteredServices.map((product: Service, index: number) => {
-        return (
-          <ServiceCard
-            key={"our-services-card-" + index + "-key"}
-            title={product.title}
-            description={product.description}
-            image={product.coverImage}
-            showButton
-            showBackground={false}
-            buttonText={"View More"}
-            reverse={index % 2 !== 0}
-            link={getLink(product)}
-            externalLink={false}
-          />
-        )})}
+        ))}
       </div>
 
+      <div className="flex flex-col gap-8 sm:gap-2 ">
+        {filteredServices.map((product: Service, index: number) => {
+          return (
+            <ServiceCard
+              key={"our-services-card-" + index + "-key"}
+              title={product.title}
+              description={product.description}
+              image={product.coverImage}
+              showButton
+              showBackground={false}
+              buttonText={"View More"}
+              reverse={index % 2 !== 0}
+              link={getLink(product)}
+              externalLink={false}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ServicesContent
+export default ServicesContent;
